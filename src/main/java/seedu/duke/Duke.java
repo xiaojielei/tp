dd3f43b188835cc0cff5a2c2733cc87f72da383d
Yshinprograms <82699007+Yshinprograms@users.noreply.github.com>
2025-03-14 22:36:28 +0800

    Readded Duke file

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -7,19 +7,23 @@ import ui.HelpDisplay;
 import java.util.Scanner;
 
 public class Duke {
+    /**
+     * Main entry-point for the java.duke.Duke application.
+     */
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
         System.out.println("What is your name?");
-
-        Scanner in = new Scanner(System.in);
-        String name = in.nextLine();
-
-        System.out.println("Hello " + name + "!");
 
         Summary summary = new Summary();
         SummaryDisplay summaryDisplay = new SummaryDisplay(summary);
         HelpDisplay helpDisplay = new HelpDisplay();
 
-        in.close();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hello " + in.nextLine());
     }
 }
