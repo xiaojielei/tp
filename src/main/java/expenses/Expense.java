381970c4280810ea27798d209ea69395e854cb1b
hannahtay <heyynuhh@gmail.com>
2025-03-24 13:40:22 +0800

    Add categories for expenses

--- src/main/java/expenses/Expense.java
+++ src/main/java/expenses/Expense.java
@@ -1,12 +1,21 @@
 package expenses;
 
+import exceptions.BudgetTrackerException;
+
 public class Expense {
+
+    public enum Category {
+        FOOD, TRANSPORT, BILLS, OTHERS
+    }
+
     private double amount;
     private String description;
+    private Category category;
 
-    public Expense(double amount, String description) {
+    public Expense(double amount, String description, Category category) {
         this.amount = amount;
         this.description = description;
+        this.category = category;
     }
 
     public double getAmount() {
@@ -17,8 +26,27 @@ public class Expense {
         return description;
     }
 
+    public Category getCategory() {
+        return category;
+    }
+
     @Override
     public String toString() {
-        return "Amount: $" + amount + " | Source: " + description;
+        return "[" + category + "] " + amount + " from " + description;
+    }
+
+    public static Category getCategoryFromInput(String input) throws BudgetTrackerException {
+        switch (input.toUpperCase()) {
+        case "F":
+            return Category.FOOD;
+        case "T":
+            return Category.TRANSPORT;
+        case "B":
+            return Category.BILLS;
+        case "O":
+            return Category.OTHERS;
+        default:
+            throw new BudgetTrackerException("Invalid category! Use: F (Food), T (Transport), B (Bills), O (Others).");
+        }
     }
 }
