dd42294cb7493b465542b3f5a2038e196be90aea
Xiao Jielei <142099098+xiaojielei@users.noreply.github.com>
2025-03-27 14:59:42 +0800

    Just now accidentaly changed SummaryDisplay, now changed back

--- src/main/java/summary/ui/SummaryDisplay.java
+++ src/main/java/summary/ui/SummaryDisplay.java
@@ -28,7 +28,7 @@ public class SummaryDisplay {
         sb.append(SUMMARY_HEADER).append("\n");
         sb.append(String.format("%-20s $%.2f%n", INCOME_LABEL, summary.getTotalIncome()));
         sb.append(String.format("%-20s $%.2f%n", EXPENSES_LABEL, summary.getTotalExpense()));
-        sb.append(String.format("%-20s $%.2f%n", BALANCE_LABEL, summary.getTotalBalance())); // getAvailableFund
+        sb.append(String.format("%-20s $%.2f%n", BALANCE_LABEL, summary.getAvailableFunds()));
         sb.append(String.format("%-20s $%.2f%n", SAVINGS_LABEL, summary.getTotalSavings()));
         sb.append(SUMMARY_FOOTER);
         
